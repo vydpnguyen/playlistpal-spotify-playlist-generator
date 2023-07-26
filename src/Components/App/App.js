591053d77d0888f962b86da6e2ab4c78e5566c92
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-26 23:13:21 +0700

    Add new folders for static components

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,24 +1,23 @@
-//import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Playlist<span className="highlight">Pal</span>
+        </h1>
+        <div className="App">
+          {/*  <!-- Add a SearchBar component --> */}
+          <div className="App-playlist">
+            {/*  <!-- Add a SearchResults component --> */}
+            {/*  <!-- Add a Playlist component --> */}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
