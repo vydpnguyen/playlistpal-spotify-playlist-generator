e1ecfa44db75bd8daafd4f0eef38a1ae92d25da4
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-27 00:06:10 +0700

    Add SearchBar

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 class App extends React.Component {
   render() {
@@ -8,8 +9,12 @@ class App extends React.Component {
         <h1>
           Playlist<span className="highlight">Pal</span>
         </h1>
+        <h3>
+          Create a custom playlist saved to your Spotify account.
+        </h3>
         <div className="App">
           {/*  <!-- Add a SearchBar component --> */}
+          <SearchBar />
           <div className="App-playlist">
             {/*  <!-- Add a SearchResults component --> */}
             {/*  <!-- Add a Playlist component --> */}
