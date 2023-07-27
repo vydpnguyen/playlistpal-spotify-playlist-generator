659c3ce707b89f4401a32afab17b5410b63f3367
Vy Nguyen <vydpnguyen@gmail.com>
2023-07-27 23:22:37 +0700

    Add function to handle playlist name change

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -53,6 +53,7 @@ class App extends React.Component {
     };
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
   }
 
   addTrack(track) {
@@ -66,6 +67,10 @@ class App extends React.Component {
   removeTrack(track) {
     let updatedPlaylist = this.state.playlistTracks.filter(playlistTrack => playlistTrack.id !== track.id);
     this.setState({ playlistTracks: updatedPlaylist });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name });
   }
 
   render() {
@@ -89,7 +94,9 @@ class App extends React.Component {
             <Playlist
               playlistName={this.state.playlistName}
               playlistTracks={this.state.playlistTracks}
-              onRemove={this.removeTrack}/>
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              />
           </div>
         </div>
       </div>
--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,12 +3,25 @@ import { TrackList } from '../TrackList/TrackList';
 import './Playlist.css';
 
 export class Playlist extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleNameChange = this.handleNameChange.bind(this);
+    }
+
+    handleNameChange(event) {
+        this.props.onNameChange(event.target.value);
+    }
+
     render() {
         return(
             <div className='Playlist'>
-                <input defaultValue={'New Playlist'}/>
+                <input defaultValue={'New Playlist'} onChange={this.handleNameChange}/>
                 {/* <!-- Add TrackList Component-->*/}
-                <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} isRemoval={true}/>
+                <TrackList
+                    tracks={this.props.playlistTracks}
+                    onRemove={this.props.onRemove}
+                    isRemoval={true}
+                />
                 <button className='Playlist-save'>Save Playlist</button>
             </div>
         );
